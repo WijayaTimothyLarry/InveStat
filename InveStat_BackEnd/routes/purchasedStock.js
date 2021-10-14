0f0c2c92e37c436e6bbfc691eae38733829d5d3b
Wu Jun Hui <junhuihuii@gmail.com>
2021-10-14 13:21:25 +0800

    backend-purchasedStock/transaction/watchlist
    delete function still needed for purchasedStock, transaction and watchlist

--- InveStat_BackEnd/routes/purchasedStock.js
+++ InveStat_BackEnd/routes/purchasedStock.js
@@ -2,21 +2,27 @@ const express = require("express");
 const router = express.Router();
 const { purchasedStock } = require("../models"); 
 
-
 router.get("/", async(req, res) => {
-  console.log(req.body);
-  const reqInfo = req.body
-  const portfolioId = reqInfo.portfolioId
-  const listOfPurchasedStocks = await purchasedStock.findAll({ where: { portfolioPortfolioId: portfolioId} });
-  res.json(listOfPurchasedStocks);
+  const ListOfPurchasedStocks = await purchasedStock.findAll()
+  res.json(ListOfPurchasedStocks);
 });
 
-
-router.post("/", async(req, res) => {
-  const stockInfo = req.body;
-  await purchasedStock.create(stockInfo);  
-  res.json(stockInfo);
-})
+router.post("/", async (req, res) => {
+  const purchasedStockInfo = req.body;
+  console.log(purchasedStockInfo);
+  console.log(purchasedStockInfo.stockTickerId);
+  console.log(purchasedStockInfo.stockName);
+  console.log(purchasedStockInfo.totalQuantity);
+  console.log(purchasedStockInfo.avgPurchasePriceUsd);
+  // avgPurchasedPriceUsd = (transactionPrice * changeInQuantity * exchangeRate)/totalQuantity
+  // calculation method not yet set up
+  console.log(purchasedStockInfo.exchangeRate);
+  console.log(purchasedStockInfo.portfolioPortfolioId);
+  // foreign key issue not resolved
+  
+  await purchasedStock.create(purchasedStockInfo);
+  res.json(purchasedStockInfo);
+});
 
 
 module.exports = router;
