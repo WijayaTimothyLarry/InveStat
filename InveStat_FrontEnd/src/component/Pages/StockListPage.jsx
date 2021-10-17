02dc4d79834a2967a08a863659ade95447cd8b2c
WijayaTimothyLarry <wijayatimothylarry@gmail.com>
2021-10-17 16:41:08 +0800

    custom select

--- InveStat_FrontEnd/src/component/Pages/StockListPage.jsx
+++ InveStat_FrontEnd/src/component/Pages/StockListPage.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import _ from "lodash";
 import { paginate } from "./../utils/paginate";
 import SearchBox from "../common/searchBox";
