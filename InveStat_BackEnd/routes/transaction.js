385a249099365c1030ff2165c2d1db9a4f964207
aileen laksmono lie <aileen31.al@gmail.com>
2021-10-19 02:00:27 +0800

    enhancement : purchasestock, transaction

--- InveStat_BackEnd/routes/transaction.js
+++ InveStat_BackEnd/routes/transaction.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { transaction } = require("../models"); 
-const { purchasedStock } = require("../models"); 
+const {
+  transaction
+} = require("../models");
+var createPStock = require("../middleware/createStock");
+
+
 
 //get
-router.get("/", async(req, res) => {
+router.get("/", async (req, res) => {
   const listOfTransactions = await transaction.findAll()
   res.json(listOfTransactions);
 });
@@ -15,58 +19,25 @@ router.get("/", async(req, res) => {
 router.post("/", createPStock, async (req, res) => {
   const transactionInfo = req.body;
   var changeInQuantity = transactionInfo.changeInQuantity;
-  if (transactionInfo.transactionType=='Sell'){
-      changeInQuantity = -1 * parseInt(transactionInfo.changeInQuantity);
-      
+  console.log(transactionInfo.brokerageCost);
+  if (transactionInfo.transactionType == 'Sell') {
+    changeInQuantity = -1 * parseInt(transactionInfo.changeInQuantity);
+
   };
   TransactionDetail = await transaction.create({
-    transactionType : transactionInfo.transactionType,
-    changeInQuantity : changeInQuantity,
-    transactionDate : transactionInfo.transactionDate,
-    purchasedStockId : res.purchasedStockId,
-    portfolioId : transactionInfo.portfolioId,
-    TransactionPrice : transactionInfo.TransactionPrice,
-    purchasedStockStockTickerId:transactionInfo.purchasedStockStockTickerId,
-    brokerageCost : transactionInfo.brokerageCost
+    transactionType: transactionInfo.transactionType,
+    changeInQuantity: changeInQuantity,
+    transactionDate: transactionInfo.transactionDate,
+    purchasedStockId: res.purchasedStockId,
+    portfolioId: transactionInfo.portfolioId,
+    TransactionPrice: transactionInfo.TransactionPrice,
+    purchasedStockStockTickerId: transactionInfo.purchasedStockStockTickerId,
+    brokerageCost: transactionInfo.brokerageCost
   });
   res.json(TransactionDetail);
 });
 
 
-async function createPStock (req,res,next){
-  const purchasedStockInfo = req.body;
-  const portfolioId = purchasedStockInfo.portfolioId;
-  const stockTickerId = purchasedStockInfo.purchasedStockStockTickerId; 
-  console.log(stockTickerId);
-  var existingStock;
-  try{
-     existingStock = await purchasedStock
-    .findOne({
-      where: {
-        portfolioId : portfolioId,
-        stockTickerId : stockTickerId
-      },
-    }).catch((e) => {
-      console.log(e.message);
-    });
-  }catch (err) {
-    console.log('err');
-  }
 
-  if (existingStock=== null) {
-    new_stock = await purchasedStock.create({
-    "stockTickerId" :stockTickerId,
-    "portfolioId" : portfolioId
-  });
-
-  res.purchasedStockId= new_stock.id;
-  console.log('new stock created');
-}
-else{
-  res.purchasedStockId= existingStock.id;
-  console.log('existing stock exists');
-}
-  next()
-}
 
 module.exports = router;
