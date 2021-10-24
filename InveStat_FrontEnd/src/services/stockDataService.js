ad0e0fdd7a957447614cdd031f55b56ae7826096
WijayaTimothyLarry <wijayatimothylarry@gmail.com>
2021-10-24 14:43:37 +0800

    Update stockDataService.js

--- InveStat_FrontEnd/src/services/stockDataService.js
+++ InveStat_FrontEnd/src/services/stockDataService.js
@@ -10,4 +10,12 @@ export async function getDailyStockData(tickerId) {
   return data["0"];
 }
 
+export async function getStockHistoricalData(tickerId) {
+  const apiEndpoint =
+    "https://financialmodelingprep.com/api/v3/historical-price-full/" +
+    tickerId +
+    "?serietype=line&apikey=7aa87da7ef549544cc1ed7281de197b0";
+  const { data } = await http.get(apiEndpoint);
+  return data;
+}
 export default { getDailyStockData };
