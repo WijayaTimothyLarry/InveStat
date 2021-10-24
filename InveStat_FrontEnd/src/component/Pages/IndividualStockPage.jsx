97d33d00ab2c31d4c99ae362b2a796fe22e6a5c6
WijayaTimothyLarry <wijayatimothylarry@gmail.com>
2021-10-24 14:49:44 +0800

    react chart js

--- InveStat_FrontEnd/src/component/Pages/IndividualStockPage.jsx
+++ InveStat_FrontEnd/src/component/Pages/IndividualStockPage.jsx
@@ -1,29 +1,23 @@
 import React, { Component } from "react";
+import { getStockHistoricalData } from "../../services/stockDataService";
 import StockGraph from "../common/stockgraph";
-import stockDataService from "./../../services/stockDataService";
 
 class IndividualStockPage extends Component {
   state = {
     stockData: [],
   };
 
-  //async componentDidMount() {
-  //  
-  //  const tickerID = this.props.match.params.ticker;
-  //  const { data } = await stockDataService.getDailyStockData(tickerID);
-  //  const dailyData = data["Time Series (Daily)"];
-  //  const stockData = Object.entries(dailyData).map(([k, v]) => {
-  //    return { date: k, price: v["5. adjusted close"] };
-  //  });
-  //  console.log(stockData);
-  //  this.setState({ stockData });
-  //}
+  async componentDidMount() {
+    const tickerID = this.props.match.params.ticker;
+    const data = getStockHistoricalData(tickerID);
+    console.log(data);
+  }
 
   render() {
     return (
       <React.Fragment>
         <h1>{this.props.match.params.ticker}</h1>
-        <StockGraph/>
+        <StockGraph />
       </React.Fragment>
     );
   }
