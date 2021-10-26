6beec006a661cf23ebce14ae111d18dedd3fe25a
WijayaTimothyLarry <wijayatimothylarry@gmail.com>
2021-10-26 23:04:46 +0800

    calculate portfolio PnL and total value

--- InveStat_FrontEnd/src/services/portfolioService.js
+++ InveStat_FrontEnd/src/services/portfolioService.js
@@ -1,12 +1,34 @@
 import http from "./httpService";
 import { apiUrl } from "../config.json";
+import purchasedStockService from "./purchasedStockService";
+import stockDataService from "./stockDataService";
+
 const apiEndpoint = apiUrl + "/portfolio";
 
 export async function getPortfolioList(token) {
-  const { data } = await http.get(apiEndpoint, {
+  const { data: portfolioList } = await http.get(apiEndpoint, {
     headers: { "x-access-token": token },
   });
-  return data;
+
+  console.log(portfolioList);
+  for (const portfolio of portfolioList) {
+    portfolio.totalValue = 0;
+    portfolio.costPrice = 0;
+    const portfolioId = portfolio.id;
+    const { data: stockList } =
+      await purchasedStockService.getPurchasedStockList(portfolioId);
+    for (const stock of stockList) {
+      const ticker = stock.stockTickerId;
+      const data = await stockDataService.getStockQuote(ticker);
+      stock.costPrice = stock.avgPurchasePriceUsd * stock.totalQuantity;
+      portfolio.costPrice += stock.costPrice;
+      stock.value = data.price * stock.totalQuantity;
+      portfolio.totalValue += stock.value;
+    }
+    portfolio.totalValue = portfolio.totalValue.toFixed(2);
+    portfolio.PnL = (portfolio.totalValue - portfolio.costPrice).toFixed(2);
+  }
+  return portfolioList;
 }
 
 export async function addNewPortfolio(userEmail, portfolioName) {
