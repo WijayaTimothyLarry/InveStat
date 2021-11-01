20508f8ad2d23a276f9f8f3db7c2c7787e47618f
WijayaTimothyLarry <wijayatimothylarry@gmail.com>
2021-11-01 13:35:16 +0800

    refactor

--- InveStat_BackEnd/routes/Login.js
+++ InveStat_BackEnd/routes/Login.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const {
-  user
-} = require("../models");
+const { user } = require("../models");
 const bcrypt = require("bcrypt");
 const JWT_SECRET = "test";
-
 
 function verifyJWT(req, res, next) {
   const token = req.headers["x-access-token"];
@@ -17,7 +14,7 @@ function verifyJWT(req, res, next) {
       if (err) {
         res.json({
           auth: false,
-          message: "u failed to authenticate"
+          message: "u failed to authenticate",
         });
       } else {
         req.user = decoded.email;
@@ -28,61 +25,59 @@ function verifyJWT(req, res, next) {
   }
 }
 
-
 router.get("/", verifyJWT, (req, res) => {
   console.log("successful authentication");
   console.log(req.user);
   res.send("you are authenticated");
 });
 
-
 router.post("/", async (req, res) => {
-  const {
-    email,
-    password
-  } = req.body;
+  const { email, password } = req.body;
 
-  const currentUser = await user.findOne({
-    where: {
-      email: email
-    }
-  }).catch((e) => {
-    console.log(e.message);
-  });
+  const currentUser = await user
+    .findOne({
+      where: {
+        email: email,
+      },
+    })
+    .catch((e) => {
+      console.log(e.message);
+    });
 
-  try{
-  bcrypt.compare(password, currentUser.password, function (err, result) {
-    // result == true
-    if (result) {
-      const id = currentUser.email;
-      const name = currentUser.name;
-      const token = jwt.sign({
-        email: id,
-        name: name
-      }, JWT_SECRET, {
-        expiresIn: "1h",
-      });
-      res.json({
-        auth: true,
-        token: token,
-        currentUser: currentUser
-      });
-    } else {
-      console.log("wrong password");
-      res.json({
-        auth: false,
-        message: "wrong password"
-      });
-    }
-    
-  });
-  delete currentUser.password;
-  console.log("successful_login");
-}
-catch{
-  res.json({"message":"no current user"});
-}
-  
+  try {
+    bcrypt.compare(password, currentUser.password, function (err, result) {
+      // result == true
+      if (result) {
+        const id = currentUser.email;
+        const name = currentUser.name;
+        const token = jwt.sign(
+          {
+            email: id,
+            name: name,
+          },
+          JWT_SECRET,
+          {
+            expiresIn: "1h",
+          }
+        );
+        res.json({
+          auth: true,
+          token: token,
+          currentUser: currentUser,
+        });
+      } else {
+        console.log("wrong password");
+        res.json({
+          auth: false,
+          message: "Wrong password",
+        });
+      }
+    });
+    delete currentUser.password;
+    console.log("successful_login");
+  } catch {
+    res.json({ message: "Email is not registered" });
+  }
 });
 
 module.exports = router;
